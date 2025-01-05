cececf154667effe8d2ffe4298456e6c49754fca
Piyush Jha <151352884+PiyushJha-git@users.noreply.github.com>
2025-01-05 18:45:35 +0530

    Delete src/UserRepository.java

--- src/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.healthcaremanagement.repository;
-
-import com.example.healthcaremanagement.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
