4e1ba626d51b31363d45977ca7c9bcc70dd07679
Piyush Jha <151352884+PiyushJha-git@users.noreply.github.com>
2025-01-05 18:46:52 +0530

    Delete src/users_data.sql

--- src/users_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO user (id, name, email, password, role) VALUES (1, 'Admin User', 'admin@example.com', 'adminpass', 'ADMIN');
-INSERT INTO user (id, name, email, password, role) VALUES (2, 'Doctor User', 'doctor@example.com', 'doctorpass', 'DOCTOR');
-INSERT INTO user (id, name, email, password, role) VALUES (3, 'Patient User', 'patient@example.com', 'patientpass', 'PATIENT');
