32a47802bdef77367c2a2f5f61d6e63bf201ce16
Piyush Jha <151352884+PiyushJha-git@users.noreply.github.com>
2025-01-05 19:12:40 +0530

    Create PatientDao.java

---
+++ target/classes/com/dao/PatientDao.java
@@ -0,0 +1,13 @@
+package com.healthcare.management.dao;
+
+import com.healthcare.management.model.Patient;
+import java.util.List;
+import java.util.Optional;
+
+public interface PatientDao {
+    List<Patient> getAllPatients();
+    Optional<Patient> getPatientById(Long id);
+    Patient savePatient(Patient patient);
+    void deletePatient(Long id);
+    Patient updatePatient(Long id, Patient patientDetails);
+}
